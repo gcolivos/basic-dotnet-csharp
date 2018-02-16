25c8852321700d44aec18643a55aa418c790f28b
Guillermo Olivos <gcolivos@gmail.com>
2018-02-16 08:58:09 -0600

    first commit please

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace list_quickstart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   Console.WriteLine($"Hello World!");
+            var names = new List<string> { "Guillermo", "Luke", "Tyler" };
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+            Console.WriteLine();
+            //can add or remove names to a list using below method
+            names.Add("Maria");
+            names.Add("Bill");
+            names.Remove("Ana");
+            //Example of lopping through a List below
+            foreach (var name in names)
+            {
+                Console.WriteLine($"My name is {name.ToUpper()}");
+            }
+            Console.WriteLine($"My real name is {names[0]}");
+        }
+    }
+}
---
+++ readMe.md
@@ -0,0 +1,8 @@
+wonderlake:~ guillermoolivos$ cd myApp
+wonderlake:myApp guillermoolivos$ code .
+wonderlake:myApp guillermoolivos$ dotnet run
+Hello World!
+wonderlake:myApp guillermoolivos$ dotnet run
+Hello Guillermo!
+
+
